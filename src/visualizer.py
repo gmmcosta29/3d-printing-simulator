53fa977794c2ac21910c1eef2ac5d1653d5aafa2
Guilherme Costa <gmmcosta29@gmail.com>
2025-09-30 19:49:26 +0100

    Plt finished

--- src/visualizer.py
+++ src/visualizer.py
@@ -2,16 +2,17 @@ import matplotlib.pyplot as plt
 from pathlib import Path
 
 class Visualizer:
-    """Manages job history persistence"""
+    """Create visualization of printer utilization"""
 
-    def __init__(self, db_path: str = "logs"):
-        self.db_path = Path(db_path)
-        self.db_path.parent.mkdir(exist_ok=True)
+    def __init__(self, dir: str = "logs"):
+        self.output_dir = Path(dir)
+        self.output_dir.parent.mkdir(exist_ok=True)
         
     def plot_printer_utilization(self, stats: dict) -> None:
+        """Plot printer utilization"""
         printer_util = stats['printer_utilization']
 
-        printer_ids = [p['printer_id'] for p in printer_util]
+        printer_ids = [f"Printer {p['printer_id']}" for p in printer_util]
         printer_utilization = [p['utilization_percent'] for p in printer_util]
 
         plt.figure(figsize=(10, 6))
@@ -22,4 +23,7 @@ class Visualizer:
         plt.ylabel('Utilization %')
         plt.ylim(1,110)
         plt.tight_layout()
-        plt.show()
+
+        filepath = self.output_dir / 'printer_utilization.png'
+        plt.savefig(filepath,dpi = 150, bbox_inches= 'tight')
+        plt.close()
