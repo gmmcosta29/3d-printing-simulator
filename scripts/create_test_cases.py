248017b6ad9bc550450e74cf3d0913c19613ee71
Guilherme Costa <gmmcosta29@gmail.com>
2025-10-01 13:46:58 +0100

    script to create test_cases

---
+++ scripts/create_test_cases.py
@@ -0,0 +1,38 @@
+
+import json 
+import random
+from pathlib import Path
+from datetime import datetime
+
+
+MATERIALS = ["PLA", "PETG", "ABS", "TPU"]
+
+def create_test_job(count: int) -> list[dict]:
+    jobs = []
+    for i in range(count):
+        jobs.append({
+            "id": f"P{i}",
+            "material": random.choice(MATERIALS),
+            "est_time": random.randint(10,40),
+            "priority": random.randint(0,5)
+        })
+    return jobs
+
+
+def main():
+    jobs = create_test_job(10)
+    data = {"jobs": jobs}
+
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    filename = f"job_report_{timestamp}.json"
+
+    dirpath = Path("test_data")
+    dirpath.mkdir(exist_ok=True)
+
+    filepath = dirpath / filename
+
+    with open(filepath,'w', encoding='utf-8') as f:
+        json.dump(data,f, indent=2)
+            
+if __name__ == "__main__":
+    main()
